7fffedc473c096292b571bc61c8f1fa9cbe27e62
Raphael <raphael.kropp@web.de>
2024-08-18 19:23:21 +0200

    Ü3

--- src/dsa3/A2/NetworkNode.java
+++ src/dsa3/A2/NetworkNode.java
@@ -6,13 +6,18 @@ import java.util.List;
 
 public class NetworkNode extends Node {
 
+    private final int id;
+    private int count;
     private List<String> connections;
+    private double majority;
 
-    public NetworkNode(int id){
-        super("Node"+id);
+    public NetworkNode(int id) {
+        super("Node" + id);
+        this.id = id;
     }
 
     public void setConnections(List<String> connections) {
         this.connections = connections;
+        this.majority = ((double) connections.size()) / 2;
     }
 }
--- src/dsa3/A2/Paxos.java
+++ src/dsa3/A2/Paxos.java
@@ -1,9 +1,51 @@
 package dsa3.A2;
 
-public class Paxos {
+import org.oxoo2a.sim4da.Message;
 
-    public Paxos(){
+public class Paxos {
+    private int highestProposalNumber;
+    private int networkSize;
+    private int nodeId;
+
+    public Paxos(int nodeId, int networkSize) {
+        this.highestProposalNumber = 0;
+        this.networkSize = networkSize;
+        this.nodeId = nodeId;
 
     }
 
+    public void sendPropose(String content) {
+        this.highestProposalNumber = (this.highestProposalNumber / this.networkSize) * this.networkSize + this.nodeId;
+        // broadcast
+    }
+
+    public void handle(Message message) {
+        Command c = Command.valueOf(message.queryHeader("type"));
+        switch (c) {
+            case Command.PREPARE:
+                break;
+            case Command.PROMISE:
+                // Handle PROMISE messages (simplified)
+                break;
+            case Command.PROPOSE:
+                break;
+            case Command.ACCEPT:
+                // handle ACCEPT
+                break;
+            case Command.LEARN:
+                // learn
+                break;
+            default:
+                throw new IllegalArgumentException("Unknown message type: " + c);
+        }
+    }
+
+}
+
+enum Command {
+    PREPARE,
+    PROMISE,
+    PROPOSE,
+    ACCEPT,
+    LEARN
 }
